0aab81884c57b50b2e36a2050bf03afdf58b1a89
Louis Pearson <louispearson@librem.one>
2022-04-28 12:38:05 -0600

    Mess around with build scripts

--- build.zig
+++ build.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+const GitRepoStep = @import("tools/GitRepoStep.zig");
+pub const BundleStep = @import("tools/BundleStep.zig");
+
 const Pkg = std.build.Pkg;
 const FileSource = std.build.FileSource;
 
@@ -25,7 +28,7 @@ pub fn addWasm4RunStep(b: *std.build.Builder, name: []const u8, cart: *std.build
     run.dependOn(&w4native.step);
 }
 
-pub fn addWasmOpt(b: *std.build.Builder, name: []const u8, cart: *std.build.LibExeObjStep) !*std.build.Step {
+pub fn addWasmOpt(b: *std.build.Builder, name: []const u8, cart: *std.build.LibExeObjStep) !*std.build.LibExeObjStep {
     const prefix = b.getInstallPath(.lib, "");
     const opt = b.addSystemCommand(&[_][]const u8{
         "wasm-opt",
@@ -36,7 +39,7 @@ pub fn addWasmOpt(b: *std.build.Builder, name: []const u8, cart: *std.build.LibE
     });
 
     opt.addArtifactArg(cart);
-    const cartname = try std.fmt.allocPrint(b.allocator, "{s}{s}", .{ name, ".wasm" });
+    const cartname = try std.fmt.allocPrint(b.allocator, "{s}-opt{s}", .{ name, ".wasm" });
     defer b.allocator.free(cartname);
     const optout = try std.fs.path.join(b.allocator, &.{ prefix, cartname });
     defer b.allocator.free(optout);
@@ -44,12 +47,17 @@ pub fn addWasmOpt(b: *std.build.Builder, name: []const u8, cart: *std.build.LibE
 
     const stepname = try std.fmt.allocPrint(b.allocator, "{s}-opt", .{name});
     defer b.allocator.free(stepname);
-    const msg = try std.fmt.allocPrint(b.allocator, "Run wasm-opt on {s}, producing {s}", .{cart.name, cartname});
+    const msg = try std.fmt.allocPrint(b.allocator, "Run wasm-opt on {s}, producing {s}", .{ cart.name, cartname });
     defer b.allocator.free(msg);
     const opt_step = b.step(stepname, msg);
     opt_step.dependOn(&cart.step);
     opt_step.dependOn(&opt.step);
-    return opt_step;
+
+    const file = try b.allocator.create(std.build.GeneratedFile);
+    file.* = std.build.GeneratedFile{ .step = opt_step, .path = optout };
+    const object = b.addObjectSource("opt", std.build.FileSource{ .generated = file });
+
+    return object;
 }
 
 pub fn addWasm4Cart(b: *std.build.Builder, name: []const u8, path: []const u8) !*std.build.LibExeObjStep {
@@ -85,29 +93,71 @@ pub fn build(b: *std.build.Builder) !void {
 
     try tests(b, mode);
 
-    const zowOS = try addWasm4Cart(b, "zowOS", "examples/zow-os/main.zig");
-    zowOS.addPackage(pkgs.zow4);
-    try addWasm4RunStep(b, "run-zowOS", zowOS);
-    const zowOS_opt = try addWasmOpt(b, "zowOS", zowOS);
+    // const zowOS = try addWasm4Cart(b, "zowOS", "examples/zow-os/main.zig");
+    // zowOS.addPackage(pkgs.zow4);
+    // try addWasm4RunStep(b, "run-zowOS", zowOS);
+    // const zowOS_opt = try addWasmOpt(b, "zowOS", zowOS);
 
     const counter = try addWasm4Cart(b, "counter", "examples/counter.zig");
     counter.addPackage(pkgs.zow4);
     try addWasm4RunStep(b, "run-counter", counter);
-    const counter_opt = try addWasmOpt(b, "counter", counter);
+    // const counter_opt = try addWasmOpt(b, "counter", counter);
 
-    const bezier = try addWasm4Cart(b, "bezier", "examples/bezier.zig");
-    bezier.addPackage(pkgs.zow4);
-    try addWasm4RunStep(b, "run-bezier", bezier);
-    const bezier_opt = try addWasmOpt(b, "bezier", bezier);
+    // const bezier = try addWasm4Cart(b, "bezier", "examples/bezier.zig");
+    // bezier.addPackage(pkgs.zow4);
+    // try addWasm4RunStep(b, "run-bezier", bezier);
+    // const bezier_opt = try addWasmOpt(b, "bezier", bezier);
 
-    const optimize_step = b.step("opt", "Builds example carts and optimizes them with wasm-opt (must be installed)");
-    optimize_step.dependOn(zowOS_opt);
-    optimize_step.dependOn(counter_opt);
-    optimize_step.dependOn(bezier_opt);
-    if (mode == .ReleaseSmall) {
-        b.getInstallStep().dependOn(optimize_step);
-        try optimize_step.make();
-    }
+    // const optimize_step = b.step("opt", "Builds example carts and optimizes them with wasm-opt (must be installed)");
+    // optimize_step.dependOn(zowOS_opt);
+    // optimize_step.dependOn(counter_opt);
+    // optimize_step.dependOn(bezier_opt);
+    // if (mode == .ReleaseSmall) {
+    //     b.getInstallStep().dependOn(optimize_step);
+    //     try optimize_step.make();
+    // }
+
+    const wasm4_repo = GitRepoStep.create(b, .{
+        .url = "https://github.com/aduros/wasm4",
+        .branch = "main",
+        .sha = "a92b8ee4d2c2bb4344619a8360138a3bbc0caf20",
+    });
+
+    // TODO: Make a compilation script for all of wasm4's dependencies. Possibly make a
+    // fork of wasm4 with zig build included
+    const wasm4_exe = b.addExecutable("wasm4", null);
+    wasm4_exe.linkLibC();
+    wasm4_exe.addIncludePath("dep/wasm4/runtimes/native/vendor/cubeb/include/");
+    wasm4_exe.addIncludePath("dep/wasm4/runtimes/native/vendor/wasm3/src/");
+    wasm4_exe.addIncludePath("dep/wasm4/runtimes/native/vendor/minifb/include/");
+    wasm4_exe.addCSourceFiles(
+        &.{
+            "dep/wasm4/runtimes/native/src/backend/main.c",
+            "dep/wasm4/runtimes/native/src/apu.c",
+            "dep/wasm4/runtimes/native/src/framebuffer.c",
+            "dep/wasm4/runtimes/native/src/runtime.c",
+          },
+        &.{},
+    );
+    wasm4_exe.step.dependOn(&wasm4_repo.step);
+
+    const exec_path = try std.fs.path.join(b.allocator, &[_][]const u8{
+        wasm4_repo.getPath(), // getPath will ensure step dependencies are correct
+        "runtimes",
+        "native",
+        "build",
+        "wasm4",
+    });
+    const counter_bundle = BundleStep.create(b, .{
+        .exec_path = .{ .path = exec_path },
+        .cart_path = counter.getOutputSource(),
+        .output_name = "wasm4-counter",
+    });
+    counter_bundle.step.dependOn(&wasm4_exe.step);
+    counter_bundle.step.dependOn(&counter.step);
+
+    const counter_bundle_step = b.step("bundle-counter", "Bundles counter example");
+    counter_bundle_step.dependOn(&counter_bundle.step);
 }
 
 fn tests(b: *std.build.Builder, mode: std.builtin.Mode) !void {
---
+++ tools/BundleStep.zig
@@ -0,0 +1,97 @@
+//! A small script for bundling carts on to the native WASM4 executable
+const std = @import("std");
+
+const KB = 1024;
+const MB = 1024 * KB;
+
+const BundleStep = @This();
+
+step: std.build.Step,
+builder: *std.build.Builder,
+cart_path: std.build.FileSource,
+exec_path: std.build.FileSource,
+output_name: []const u8,
+
+
+pub fn create(b: *std.build.Builder, opt: struct {
+    cart_path: std.build.FileSource,
+    exec_path: std.build.FileSource,
+    output_name: []const u8,
+}) *@This() {
+    var result = b.allocator.create(BundleStep) catch @panic("memory");
+    result.* = BundleStep {
+        .step = std.build.Step.init(.custom, "bundle a wasm4 cart with the native wasm4 runtime", b.allocator, make),
+        .builder = b,
+        .cart_path = opt.cart_path,
+        .exec_path = opt.exec_path,
+        .output_name = opt.output_name
+    };
+    return result;
+    // const bundler = b.addExecutable(opt.executable_name, opt.src_path);
+    // {
+    //     bundler.setTarget(b.standardTargetOptions(.{}));
+    //     bundler.install();
+    // }
+    // const run_bundler = bundler.run();
+    // run_bundler.step.dependOn(b.getInstallStep());
+    // {
+    //     run_bundler.addFileSourceArg(.{.path = "lib/wasm4/runtimes/native/build/wasm4"});
+    //     run_bundler.addArtifactArg(dearPrincess);
+    //     // const bundled_exe_path = b.getInstallPath(.bin, "wasm4-linux");
+    //     run_bundler.addArg(b.getInstallPath(.bin, "wasm4-linux"));
+    // }
+}
+
+const FileFooter = extern struct {
+    /// Should be the 4 byte ASCII string "CART" (1414676803)
+    magic: [4]u8,
+
+    /// Window title
+    title: [128]u8,
+
+    /// Length of the cart.wasm bytes used to offset backwards from the footer
+    cartLength: u32,
+};
+
+fn make(step: *std.build.Step) !void {
+    const this = @fieldParentPtr(BundleStep, "step", step);
+
+    const exe_src = this.exec_path.getPath(this.builder);
+    const cart_src = this.cart_path.getPath(this.builder);
+    const output = this.output_name;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const cart_file = try std.fs.openFileAbsolute(cart_src, .{});
+    defer cart_file.close();
+    const cart = try cart_file.readToEndAlloc(allocator, 1 * MB);
+    defer allocator.free(cart);
+
+    const exe_file = try std.fs.openFileAbsolute(exe_src, .{});
+    defer exe_file.close();
+    const exe = try exe_file.readToEndAlloc(allocator, 10 * MB);
+    defer allocator.free(exe);
+
+    var data = std.ArrayList(u8).init(allocator);
+    defer data.deinit();
+    const writer = data.writer();
+
+    try writer.writeAll(exe);
+    try writer.writeAll(cart);
+
+    var footer = FileFooter{
+        .magic = .{ 'C', 'A', 'R', 'T' },
+        .title = undefined,
+        .cartLength = @truncate(u32, cart.len),
+    };
+
+    _ = try std.fmt.bufPrintZ(&footer.title, "Dear Princess", .{});
+
+    try writer.writeStruct(footer);
+
+    const out_file = try std.fs.createFileAbsolute(output, .{});
+    defer out_file.close();
+    _ = try out_file.write(data.items);
+    // out_file.chmod(.executable);
+}
